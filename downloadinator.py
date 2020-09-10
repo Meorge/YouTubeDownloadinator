919c8f09abfd47e46540f27795db3cc7450ccf3f
Malcolm Anderson <malcolminyo@gmail.com>
2020-09-10 12:03:25 -0700

    it be threading

--- downloadinator.py
+++ downloadinator.py
@@ -3,12 +3,13 @@ import youtube_dl
 import re
 import threading
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
 from datetime import timedelta
 
 from typing import List
 
+import os.path
 from mutagen.id3 import ID3, TPE1, TPE2, TIT2, TRCK, TALB, APIC, TYER, TCON, TRCK
 
 
@@ -45,12 +46,14 @@ def get_names_in_playlist(url):
         print(e)
         return []
 
-class TrackItem:
-    def __init__(self, trackData={"title": "TITLE", "duration": 0}, trackIndex = (0,0)):
+class TrackItem(QtCore.QObject):
+    def __init__(self, parent, trackData={"title": "TITLE", "duration": 0}, trackIndex = (0,0)):
+        super(TrackItem, self).__init__(parent)
         self.__rawName = trackData["title"]
         self.__trackTitle = trackData["title"]
         self.__trackArtist = "ARTIST"
         self.__trackAlbum = "ALBUM"
+        self.__trackAlbumArtPath = ""
         self.__trackYear = "YEAR"
         self.__trackGenre = "Soundtrack"
         self.__duration = trackData["duration"]
@@ -59,7 +62,7 @@ class TrackItem:
 
         self.__trackIndex = trackIndex
 
-        self.__progressBar: QtWidgets.QProgressBar = None
+        self.__progressBar: ProgressIndicator = None
         self.__downloadThread = None
 
     def title(self):
@@ -74,6 +77,9 @@ class TrackItem:
     def year(self):
         return self.__trackYear
 
+    def albumArtPath(self):
+        return self.__albumArtPath
+
     def genre(self):
         return self.__trackGenre
 
@@ -85,6 +91,9 @@ class TrackItem:
 
     def readableDuration(self):
         return timedelta(seconds=self.duration())
+
+    def trackIndex(self):
+        return self.__trackIndex
 
     def url(self):
         return self.__url
@@ -100,6 +109,9 @@ class TrackItem:
 
     def setYear(self, year):
         self.__trackYear = year
+
+    def setAlbumArtPath(self, path):
+        self.__albumArtPath = path
 
     def setGenre(self, genre):
         self.__trackGenre = genre
@@ -124,27 +136,46 @@ class TrackItem:
         if d["status"] == "downloading":
             downloaded_bytes = d["downloaded_bytes"]
             total_bytes = d["total_bytes"] if "total_bytes" in d else (d["total_bytes_estimate"] if "total_bytes_estimate" in d else None)
-            self.updateProgressBar((downloaded_bytes, total_bytes))
+            self.__progressBar.updateProgressBar(f"Downloading ({int((downloaded_bytes / total_bytes) * 100)}%)", (downloaded_bytes, total_bytes))
             # print(f"Track {self.title()} is downloading: {downloaded_bytes} / {total_bytes if total_bytes is not None else '???'}")
         elif d["status"] == "error":
             print(f"Track {self.title()} had an error while downloading")
+            self.__progressBar.updateProgressBar(f"Error", (0, 1))
         elif d["status"] == "finished":
             print(f"Track {self.title()} finished downloading, and is available at {d['filename']}")
-
-
-    def updateProgressBar(self, progress):
-        if progress[1] is None: progress[1] = 0
-        self.__progressBar.setRange(progress[0], progress[1])
-        self.__progressBar.setValue(progress[0])
+            self.__progressBar.updateProgressBar(f"Processing", (0, 0))
 
     def downloadAsMP3(self):
-        self.__downloadThread = threading.Thread(target=self.downloadAsMP3Thread)
+        self.__downloadThread = TrackDownloaderThread(self)
+
+        self.__downloadThread.statusUpdated.connect(self.updateProgress)
+        self.__downloadThread.startingProcessing.connect(lambda: self.__progressBar.updateProgressBar("Processing", (0,0)))
+        self.__downloadThread.allDone.connect(lambda: self.__progressBar.updateProgressBar("Done", (1,1)))
+
         self.__downloadThread.start()
+
+
+class TrackDownloaderThread(QtCore.QThread):
+    statusUpdated = QtCore.pyqtSignal(object)
+
+    startingProcessing = QtCore.pyqtSignal()
+    allDone = QtCore.pyqtSignal()
+
+    def __init__(self, parent):
+        super(TrackDownloaderThread, self).__init__(parent)
+        self.parent = parent
+
+
+    def updateProgress(self, d):
+        self.statusUpdated.emit(d)
+
+    def run(self):
+        self.downloadAsMP3Thread()
 
     def downloadAsMP3Thread(self):
         download_mp3_options = {
             'format': 'bestaudio/best',
-            'outtmpl': f"{self.title()}.",
+            'outtmpl': f"{self.parent.album()}/{self.parent.title()}.",
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -157,36 +188,110 @@ class TrackItem:
         }
 
         with youtube_dl.YoutubeDL(download_mp3_options) as ydl:
-            ydl.download([self.url()])
+            ydl.download([self.parent.url()])
 
-        # now it's been downloaded, so let's apply the metadata!
         self.setMP3Metadata()
 
     def setMP3Metadata(self):
-        mp3_file = ID3(f"{self.title()}.mp3")
-        mp3_file['TPE1'] = TPE1(encoding=3, text=self.artist())
-        mp3_file['TPE2'] = TPE2(encoding=3, text=self.artist())
-        mp3_file['TPE3'] = TPE2(encoding=3, text=self.artist())
-        mp3_file['TIT2'] = TALB(encoding=3, text=self.title())
-        mp3_file['TALB'] = TALB(encoding=3, text=self.album())
-        mp3_file['TYER'] = TYER(encoding=3, text=self.year())
-        mp3_file['TCON'] = TCON(encoding=3, text=self.genre())
+        self.startingProcessing.emit()
+        mp3_file = ID3(f"{self.parent.album()}/{self.parent.title()}.mp3")
+        mp3_file['TPE1'] = TPE1(encoding=3, text=self.parent.artist())
+        mp3_file['TPE2'] = TPE2(encoding=3, text=self.parent.artist())
+        mp3_file['TPE3'] = TPE2(encoding=3, text=self.parent.artist())
+        mp3_file['TIT2'] = TALB(encoding=3, text=self.parent.title())
+        mp3_file['TALB'] = TALB(encoding=3, text=self.parent.album())
+        mp3_file['TYER'] = TYER(encoding=3, text=self.parent.year())
+        mp3_file['TCON'] = TCON(encoding=3, text=self.parent.genre())
 
-        if self.__trackIndex != (0,0):
-            mp3_file["TRCK"] = TRCK(encoding=3, text=f"{self.__trackIndex[0]}/{self.__trackIndex[1]}")
+        if self.parent.trackIndex() != (0,0):
+            mp3_file["TRCK"] = TRCK(encoding=3, text=f"{self.parent.trackIndex()[0]}/{self.parent.trackIndex()[1]}")
 
-        with open('SpiritOfJustice.png', 'rb') as albumart:
-            mp3_file['APIC'] = APIC(
-                          encoding=3,
-                          mime='image/png',
-                          type=3, desc=u'Cover',
-                          data=albumart.read()
-                        ) 
+        if self.parent.albumArtPath() != "":
+            with open(self.parent.albumArtPath(), 'rb') as albumart:
+                mp3_file['APIC'] = APIC(
+                            encoding=3,
+                            mime='image/png',
+                            type=3, desc=u'Cover',
+                            data=albumart.read()
+                            ) 
 
 
-        print(f"TPE1={self.artist()}, TIT2={self.title()}, TALB={self.album()}")
+        print(f"TPE1={self.parent.artist()}, TIT2={self.parent.title()}, TALB={self.parent.album()}")
         mp3_file.save()
+        self.allDone.emit()
 
+class ArtworkPicker(QtWidgets.QWidget):
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setupUI()
+        self.setArtworkPath("SpiritOfJustice.png")
+
+
+    def setupUI(self):
+        self.__layout = QtWidgets.QVBoxLayout()
+        self.__layout.setContentsMargins(0,0,0,0)
+
+        self.__horizLayout = QtWidgets.QHBoxLayout()
+        self.__filePathEditor = QtWidgets.QLineEdit()
+        self.__filePathEditor.textEdited.connect(self.setArtworkPath)
+        self.__browseButton = QtWidgets.QPushButton("Browse...")
+        self.__browseButton.clicked.connect(self.openFileLocator)
+
+        self.__horizLayout.addWidget(self.__filePathEditor)
+        self.__horizLayout.addWidget(self.__browseButton)
+
+        self.__artworkPreview = QtWidgets.QLabel("No artwork selected")
+        # self.__artworkPreview.setFrameRect(QtCore.QRect(0,0,128,128))
+
+
+        self.__layout.addLayout(self.__horizLayout)
+        self.__layout.addWidget(self.__artworkPreview)
+        self.setLayout(self.__layout)
+
+    def artworkPath(self):
+        return self.__artworkPath
+
+    def setArtworkPath(self, filename):
+        self.__artworkPath = filename
+        self.__filePathEditor.setText(filename)
+        self.updatePreview()
+
+    def updatePreview(self):
+        previewPixmap = QtGui.QPixmap(self.__artworkPath)
+        if not previewPixmap.isNull():
+            previewPixmap = previewPixmap.scaledToHeight(128, QtCore.Qt.SmoothTransformation)
+            self.__artworkPreview.setPixmap(previewPixmap)
+            self.__artworkPreview.setScaledContents(False)
+        else:
+            self.__artworkPreview.clear()
+            self.__artworkPreview.setText("Not a valid image")
+
+    def openFileLocator(self):
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self,"Select an album artwork file", "","PNG Files (*.png)")
+        if fileName:
+            self.setArtworkPath(fileName)
+        
+
+class ProgressIndicator(QtWidgets.QWidget):
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.setupUI()
+
+    def setupUI(self):
+        self.__layout = QtWidgets.QHBoxLayout()
+
+        self.__label = QtWidgets.QLabel("Idle")
+        self.__bar = QtWidgets.QProgressBar()
+
+        self.__layout.addWidget(self.__label)
+        self.__layout.addWidget(self.__bar)
+        self.setLayout(self.__layout)
+
+    def updateProgressBar(self, text, progress):
+        if progress[1] is None: progress[1] = 0
+        self.__label.setText(text)
+        self.__bar.setRange(progress[0], progress[1])
+        self.__bar.setValue(progress[0])
 
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -211,6 +316,7 @@ class MyWindow(QtWidgets.QMainWindow):
     def setUpRightHandSide(self):
         ## Set up Playlist URL box
         self.urlGroupBox = QtWidgets.QGroupBox("Playlist URL")
+        self.urlGroupBox.setSizePolicy(QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Minimum)
 
         self.urlGroupBox_lineEntry = QtWidgets.QLineEdit("https://www.youtube.com/playlist?list=PLtPXiJedNjDFfsaMKnhsJyOZH823bXzMt")
         self.urlGroupBox_goButton = QtWidgets.QPushButton(">")
@@ -223,10 +329,13 @@ class MyWindow(QtWidgets.QMainWindow):
 
         ## Set up Per-Album Data
         self.albumDataGroupBox = QtWidgets.QGroupBox("Album Information")
+        self.albumDataGroupBox.setSizePolicy(QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.MinimumExpanding)
+
         self.songNameRegexLineEdit = QtWidgets.QLineEdit("^(.*)[\s*]-")
         self.albumDataGroupBox_albumName = QtWidgets.QLineEdit("Phoenix Wright: Ace Attorney − Spirit of Justice")
         self.albumDataGroupBox_artistName = QtWidgets.QLineEdit("Capcom")
         self.albumDataGroupBox_year = QtWidgets.QLineEdit("2016")
+        self.albumDataGroupBox_artworkPicker = ArtworkPicker()
 
         self.albumDataLayout = QtWidgets.QFormLayout()
         self.albumDataLayout.setFieldGrowthPolicy(QtWidgets.QFormLayout.ExpandingFieldsGrow)
@@ -234,6 +343,7 @@ class MyWindow(QtWidgets.QMainWindow):
         self.albumDataLayout.addRow(QtWidgets.QLabel("Album"), self.albumDataGroupBox_albumName)
         self.albumDataLayout.addRow(QtWidgets.QLabel("Artist"), self.albumDataGroupBox_artistName)
         self.albumDataLayout.addRow(QtWidgets.QLabel("Year"), self.albumDataGroupBox_year)
+        self.albumDataLayout.addRow(QtWidgets.QLabel("Artwork"), self.albumDataGroupBox_artworkPicker)
         self.albumDataGroupBox.setLayout(self.albumDataLayout)
 
 
@@ -259,10 +369,11 @@ class MyWindow(QtWidgets.QMainWindow):
 
         trackIndex = 1
         for track in data:
-            self.track_list.append(TrackItem(track, (trackIndex, len(data))))
+            self.track_list.append(TrackItem(self, track, (trackIndex, len(data))))
             trackIndex += 1
 
         self.updatePreview()
+        
 
     def updatePreview(self):
         # Apply our changes to each item in the queue
@@ -270,13 +381,16 @@ class MyWindow(QtWidgets.QMainWindow):
         albumName = self.albumDataGroupBox_albumName.text()
         albumYear = self.albumDataGroupBox_year.text()
         regexPattern = self.songNameRegexLineEdit.text()
+        albumArtPath = self.albumDataGroupBox_artworkPicker.artworkPath()
         for track in self.track_list:
             track.applyRegexTitlePattern(regexPattern)
             track.setArtist(artistName)
             track.setAlbum(albumName)
             track.setYear(albumYear)
-            track.setProgressBar(QtWidgets.QProgressBar())
+            track.setAlbumArtPath(albumArtPath)
+            track.setProgressBar(ProgressIndicator())
         self.populateLeftHandSideWithTracks()
+        
 
     def populateLeftHandSideWithTracks(self):
         print("Populate list of tracks")
@@ -291,12 +405,19 @@ class MyWindow(QtWidgets.QMainWindow):
             self.leftHandQueue.addTopLevelItem(new_list_item)
             self.leftHandQueue.setItemWidget(new_list_item, 2, track.progressBar())
 
+
     def downloadTracks(self):
+        # update track metadata
+        self.updatePreview()
+
+        # check if the target directory exists, make it if not
+        albumName = self.albumDataGroupBox_albumName.text()
+        if not os.path.isdir(albumName): os.mkdir(albumName)
+
         for track in self.track_list:
             track.downloadAsMP3()
 
 
-# print(get_names_in_playlist("https://www.youtube.com/watch?v=WJyaIvAGwqI&"))
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
